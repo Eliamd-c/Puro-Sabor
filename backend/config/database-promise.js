acb001928ef3c810113c94bafb80273add929988
Eliamd-c <eliamd-c@users.noreply.github.com>
2026-06-18 10:52:56 -0500

    fix: dbAsync.run to preserve 'this' context and lastID property

--- backend/config/database-promise.js
+++ backend/config/database-promise.js
@@ -2,7 +2,14 @@ const util = require('util');
 const db = require('./database');
 
 const dbAsync = {
-  run: util.promisify(db.run.bind(db)),
+  run: function(sql, params) {
+    return new Promise((resolve, reject) => {
+      db.run(sql, params || [], function(err) {
+        if (err) reject(err);
+        else resolve(this); // 'this' contains lastID and changes
+      });
+    });
+  },
   get: util.promisify(db.get.bind(db)),
   all: util.promisify(db.all.bind(db))
 };
